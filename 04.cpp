d1de4064c9d3b2c28530092bc855ba759e1e7a57
Anshu <anshutripathi635@gmail.com>
2024-06-12 11:49:17 +0530

    default constructor

---
+++ 04.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+class Employee{
+    public:
+    Employee(){
+     cout<<"Employee constructor called ";
+            
+    }
+};
+int main(){
+    Employee e1;
+    return 0;
+}
